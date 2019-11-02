7e04c805cbc644fdf88505a2e81146d771d0eca1
Dexter <vivekojha961@gmail.com>
2019-11-02 17:53:31 +0530

    2nd

---
+++ equatewithoutXOR.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+    using namespace std;
+    
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a=6;
+    int b=5;
+    if(a^b)
+        cout<<"Not Equal";
+    else
+        cout<<"Equal";
+    return 0;
+}
